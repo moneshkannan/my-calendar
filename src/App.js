9288d7b9fae3b6197f7059366f76a70ef5b62d58
santhosh162000 <santhosh.m@codingmart.com>
2021-05-01 10:59:51 +0530

    Added loader, icon and title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
+import { useEffect } from 'react';
+import Loader from './Assets/Loader.gif';
 
 function App() {
+
+  useEffect(() => {
+    window.onload = function(){
+      document.getElementById('loader').style.display = 'none'
+    }
+  })
+
   return (
+
     <div className="App">
+
+      <div className="loader" id="loader">
+        <div className="loader_div">
+          <img src={Loader} alt="Loading..." width="250"></img>
+        </div>
+      </div>
+
       <h1>Hi My Calenly</h1>
     </div>
   );
