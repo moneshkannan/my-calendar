f69838a2f274a8c30f63b3a4d6dfefc871f8d3f5
moneshkannan <moneshkannan02@gmail.com>
2021-05-03 12:12:26 +0530

    default components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import { useEffect } from 'react';
 import Loader from './Assets/Loader.gif';
-import Navbar from './components/Navbar/Navbar';
-import Page1 from './components/Page1/Page1';
+import Navbar from './Components/Navbar/Navbar';
+import Page1 from './Components/Page1/Page1';
 
 function App() {
 
