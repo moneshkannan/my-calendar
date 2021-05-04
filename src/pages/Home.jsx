ed9a190b528b4222d04f106018e8c0f11de57b9e
santhosh162000 <santhosh.m@codingmart.com>
2021-05-04 18:59:40 +0530

    Added Footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Page1 from "../Components/Page1/Page1";
+import Footer from "../Components/Footer/Footer";
 
 function Home() {
   return (
     <div>
       <Page1 />
+      <Footer />
     </div>
   );
 }
