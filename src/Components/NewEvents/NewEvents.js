836dcc9b7dd86280060283554144b07ad7f95d4f
Rahul16-creator <rahulkannanrk1999@gmail.com>
2021-05-04 11:11:41 +0530

    warning removed

--- src/Components/NewEvents/NewEvents.js
+++ src/Components/NewEvents/NewEvents.js
@@ -4,17 +4,17 @@ import "./newevents.css";
 function NewEvents() {
     return (
         <div>
-            <div class="col-12 col-md-11 newevents_col1">
+            <div className="col-12 col-md-11 newevents_col1">
                 <a href="/" className="newevents_col1_title">New Events</a>
             </div>
             <div className="newevents newevents_item1">
                 <div className="newevents_main">
                     <div className="newevents_items">
-                        <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="meet 1" />
+                        <input type="text" readonly className="form-control-plaintext" id="staticEmail" value="meet 1" />
                     </div>
                     <div className="newevents_buttons">
-                        <button type="button" class="btn btn-success newevents_button">Accept</button>
-                        <button type="button" class="btn btn-danger newevents_button">Reject</button>
+                        <button type="button" className="btn btn-success newevents_button">Accept</button>
+                        <button type="button" className="btn btn-danger newevents_button">Reject</button>
                     </div>
                 </div>
             </div>
@@ -25,8 +25,8 @@ function NewEvents() {
                         <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="meet 2" />
                     </div>
                     <div className="newevents_buttons">
-                        <button type="button" class="btn btn-success newevents_button">Accept</button>
-                        <button type="button" class="btn btn-danger newevents_button">Reject</button>
+                        <button type="button" className="btn btn-success newevents_button">Accept</button>
+                        <button type="button" className="btn btn-danger newevents_button">Reject</button>
                     </div>
                 </div>
             </div>
@@ -34,11 +34,11 @@ function NewEvents() {
             <div className="newevents newevents_item1">
                 <div className="newevents_main">
                     <div className="newevents_items">
-                        <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="meet 3" />
+                        <input type="text" readonly className="form-control-plaintext" id="staticEmail" value="meet 3" />
                     </div>
                     <div className="newevents_buttons">
-                        <button type="button" class="btn btn-success newevents_button">Accept</button>
-                        <button type="button" class="btn btn-danger newevents_button">Reject</button>
+                        <button type="button" className="btn btn-success newevents_button">Accept</button>
+                        <button type="button" className="btn btn-danger newevents_button">Reject</button>
                     </div>
                 </div>
             </div>
@@ -46,11 +46,11 @@ function NewEvents() {
             <div className="newevents newevents_item1">
                 <div className="newevents_main">
                     <div className="newevents_items">
-                        <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="meet 4" />
+                        <input type="text" readonly className="form-control-plaintext" id="staticEmail" value="meet 4" />
                     </div>
                     <div className="newevents_buttons">
-                        <button type="button" class="btn btn-success newevents_button">Accept</button>
-                        <button type="button" class="btn btn-danger newevents_button">Reject</button>
+                        <button type="button" className="btn btn-success newevents_button">Accept</button>
+                        <button type="button" className="btn btn-danger newevents_button">Reject</button>
                     </div>
                 </div>
             </div>
@@ -58,11 +58,11 @@ function NewEvents() {
             <div className="newevents newevents_item1">
                 <div className="newevents_main">
                     <div className="newevents_items">
-                        <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="meet 5" />
+                        <input type="text" readonly className="form-control-plaintext" id="staticEmail" value="meet 5" />
                     </div>
                     <div className="newevents_buttons">
-                        <button type="button" class="btn btn-success newevents_button">Accept</button>
-                        <button type="button" class="btn btn-danger newevents_button">Reject</button>
+                        <button type="button" className="btn btn-success newevents_button">Accept</button>
+                        <button type="button" className="btn btn-danger newevents_button">Reject</button>
                     </div>
                 </div>
             </div>
