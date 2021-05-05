67c5936bae42302fc480f533abd37490197bd326
santhosh162000 <santhosh.m@codingmart.com>
2021-05-05 19:18:22 +0530

    Added contents in footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -9,29 +9,28 @@ function Footer() {
             <div className="row">
             {/* Column1 */}
             <div className="col">
-                <h4>Column 1</h4>
+                <h4>Contact</h4>
                 <ui className="list-unstyled">
-                <li>Column 1</li>
-                <li>Column 1</li>
-                <li>Column 1</li>
+                <li>Appointy Team</li>
+                <li><i className="fas fa-envelope"></i><a href="mailto:appointyteam@gmail.com" style={{textDecoration:"none", color:"white"}}> appointyteam@gmail.com </a></li>
                 </ui>
             </div>
             {/* Column2 */}
             <div className="col">
-                <h4>Column 1</h4>
+                <h4>Menu</h4>
                 <ui className="list-unstyled">
-                <li>Column 1</li>
-                <li>Column 1</li>
-                <li>Column 1</li>
+                <li>Team</li>
+                <li>Features</li>
+                <li>Contact us</li>
                 </ui>
             </div>
             {/* Column3 */}
             <div className="col">
-                <h4>Column 1</h4>
+                <h4>Connect with us</h4>
                 <ui className="list-unstyled">
-                <li>Column 1</li>
-                <li>Column 1</li>
-                <li>Column 1</li>
+                <li><i className="fab fa-linkedin"></i> LinkedIn</li>
+                <li><i className="fab fa-twitter"></i> Twitter</li>
+                <li><i className="fab fa-facebook"></i> Facebook</li>
                 </ui>
             </div>
             </div>
