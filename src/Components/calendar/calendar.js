c784b12112957897fa0ebc0107a90ffcc8d97fb6
Rahul16-creator <rahulkannanrk1999@gmail.com>
2021-05-06 16:36:04 +0530

    event deletion integration added

--- src/Components/calendar/calendar.js
+++ src/Components/calendar/calendar.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Paper from '@material-ui/core/Paper';
-import { ViewState, EditingState, IntegratedEditing} from '@devexpress/dx-react-scheduler';
+import { ViewState, EditingState, IntegratedEditing } from '@devexpress/dx-react-scheduler';
 import {
   Scheduler,
   WeekView,
@@ -16,23 +16,17 @@ import Notification from "../../service/NotificationService";
 import Navbar from '../Navbar/Navbar';
 import Loader from '../Loader/Loader';
 
-const SHIFT_KEY = 16;
 
 export default class Demo extends React.PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
-      isShiftPressed: false,
       currentDate: new Date(),
       isLoading: true,
       data: []
     };
     this.currentDateChange = (currentDate) => { this.setState({ currentDate }); };
-
-    this.commitChanges = this.commitChanges.bind(this);
-    this.onKeyDown = this.onKeyDown.bind(this);
-    this.onKeyUp = this.onKeyUp.bind(this);
   }
 
 
@@ -43,7 +37,7 @@ export default class Demo extends React.PureComponent {
     axios.get(`${API}/fetch_user_id/${userId}`).then(res => {
       let newArr = []
       res.data.response.events.forEach(element => {
-        newArr.push(element.eventDetails)
+        newArr.push({ ...element.eventDetails, id: element.eventDetails._id })
       });
       this.setState({ data: newArr })
       this.setState({ isLoading: false });
@@ -54,61 +48,38 @@ export default class Demo extends React.PureComponent {
       }
     })
   }
-  
-
-
-//  componentDidMount() {
-//         window.addEventListener('keydown', this.onKeyDown);
-//      window.addEventListener('keyup', this.onKeyUp);
-//   }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown');
-    window.removeEventListener('keyup');
-  }
-
-  onKeyDown(event) {
-    if (event.keyCode === SHIFT_KEY) {
-      this.setState({ isShiftPressed: true });
-    }
-  }
-
-  onKeyUp(event) {
-    if (event.keyCode === SHIFT_KEY) {
-      this.setState({ isShiftPressed: false });
-    }
-  }
 
 
 
-  commitChanges({ added, changed, deleted })   {
-    this.setState((state) => {
-      let { data } = state;
-      const { isShiftPressed } = this.state;
-      if (added) {
-        const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
-        data = [...data, { id: startingAddedId, ...added }];
+
+
+  commitChanges = ({ deleted }) => {
+
+    this.setState({ isLoading: true });
+
+    const token = localStorage.getItem("token");
+
+    axios.delete(`${API}/schedule/delete/${deleted}`, {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": token
       }
-      if (changed) {
-        if (isShiftPressed) {
-          const changedAppointment = data.find(appointment => changed[appointment.id]);
-          const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
-          data = [
-            ...data,
-            { ...changedAppointment, id: startingAddedId, ...changed[changedAppointment.id] },
-          ];
-        } else {
-          data = data.map(appointment => (
-            changed[appointment.id]
-              ? { ...appointment, ...changed[appointment.id] }
-              : appointment));
-        }
+    }).then(res => {
+
+      let newArr = []
+      newArr = this.state.data.filter(e => e.id !== deleted)
+
+      this.setState({ data: newArr })
+
+      Notification.show(res.data);
+      this.setState({ isLoading: false });
+    }).catch(err => {
+      if (err.response) {
+        Notification.show(err.response.data);
+        this.setState({ isLoading: false });
       }
-      if (deleted !== undefined) {
-        data = data.filter(appointment => appointment.id !== deleted);
-      }
-      return { data };
-    });
+    })
+
   }
 
 
@@ -134,9 +105,9 @@ export default class Demo extends React.PureComponent {
                 onCurrentDateChange={this.currentDateChange}
               />
               <EditingState
-            onCommitChanges={this.commitChanges}
-          />
-          <IntegratedEditing />
+                onCommitChanges={this.commitChanges}
+              />
+              <IntegratedEditing />
               <WeekView
                 startDayHour={9}
                 endDayHour={21}
@@ -146,8 +117,8 @@ export default class Demo extends React.PureComponent {
               <TodayButton />
               <Appointments />
               <AppointmentTooltip
-            showDeleteButton
-          />
+                showDeleteButton
+              />
             </Scheduler>
           </Paper>
         }
