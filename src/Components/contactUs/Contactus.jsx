c399e7fe6058354eec23cbb31f9482ed44309eef
santhosh162000 <santhosh.m@codingmart.com>
2021-05-09 17:28:20 +0530

    Updated ContactUs page

--- src/Components/contactUs/Contactus.jsx
+++ src/Components/contactUs/Contactus.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import classes from "./contactUs.module.css";
 import contact from "../../Assets/contactUs.svg";
 import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 
 const Contactus = () => {
@@ -11,22 +12,18 @@ const Contactus = () => {
       <div className={classes.contactus}>
         <div className={classes.content}>
           <div className={classes.text}>
-            <h1 className={classes.contactus_header}>Contact Us</h1>
+            <h1 className={classes.contactus_header}>We are here to help</h1>
             <p className={classes.contactUs_para}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-              corporis sunt qui incidunt praesentium dolorem quos officiis minus
-              nihil totam tempora cor Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Maxime corporis sunt qui incidunt praesentium
-              dolorem quos officiis minus nihil totam tempora cor rupti id,
-              temporibus, unde doloribus. Voluptatibus doloremque illum fuga.
+            Have questions? At Appointy, we love talking to our clients. You can reach us via email or chat
             </p>
+            <a className={classes.contactmail} href="mailto:appointyteam@gmail.com"> appointyteam@gmail.com </a>
           </div>
           <div>
             <img src={contact} className={classes.content_img} alt="" />
           </div>
         </div>
+        <Footer />
       </div>
-      
     </>
   );
 };
